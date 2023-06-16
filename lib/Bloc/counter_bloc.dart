b7b9c5d37ef5c35b3b27c9818b502ec41e98c752
ohsangsoo <tkdtn3264@gmail.com>
2023-06-16 15:33:29 +0900

    BehaviorSubject 위해서 마지막 카운트 값을 출력한다.

--- lib/Bloc/counter_bloc.dart
+++ lib/Bloc/counter_bloc.dart
@@ -1,8 +1,12 @@
-
+import 'package:rxdart/rxdart.dart';
 class CounterBloc{
   int _count =0;
 
+  // 카운터앱을 모니터링하기 위해서
+  final _countSubject = BehaviorSubject<int>();
+
   void addCounter(){
     _count++;
+    _countSubject.add(_count); // BehaviorSubject 위해서 마지막 카운트 값을 출력한다.
   }
 }
