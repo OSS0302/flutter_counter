91ebdf55f78fbac8d12880eb46514b084f08970a
ohsangsoo <tkdtn3264@gmail.com>
2023-06-17 20:11:39 +0900

    Stream 추가 해서 UI 데이터 추가

--- lib/Bloc/counter_bloc.dart
+++ lib/Bloc/counter_bloc.dart
@@ -1,15 +1,17 @@
 import 'package:rxdart/rxdart.dart';
-class CounterBloc{
-  int _count =0;
+class CounterBloc {
+  int _count = 0;
 
   // 카운터앱을 모니터링하기 위해서
   final _countSubject = BehaviorSubject<int>();
 
+  // Stream 추가
+  Stream<int> get count$ => _countSubject.stream;
+
   void addCounter(){
     _count++;
     _countSubject.add(_count); // BehaviorSubject 위해서 마지막 카운트 값을 출력한다.
+    _countSubject.value++;
   }
 
-  // Stream 추가
-Stream<int> get count$ => _countSubject.stream;
 }
